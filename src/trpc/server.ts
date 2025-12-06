65784754dadd04215d35ed9488a7d119079d4b31
Oliver Nemec <28827159+onemec@users.noreply.github.com>
2025-12-06 11:42:27 -0500

    refactor: Update tRPC context creation to be async and await headers.

--- src/trpc/server.ts
+++ src/trpc/server.ts
@@ -1,23 +1,23 @@
 import "server-only";
 
-import {createHydrationHelpers} from "@trpc/react-query/rsc";
-import {headers, type UnsafeUnwrappedHeaders} from "next/headers";
-import {cache} from "react";
+import { createHydrationHelpers } from "@trpc/react-query/rsc";
+import { headers } from "next/headers";
+import { cache } from "react";
 
-import {type AppRouter, createCaller} from "~/server/api/root";
-import {createTRPCContext} from "~/server/api/trpc";
-import {createQueryClient} from "./query-client";
+import { type AppRouter, createCaller } from "~/server/api/root";
+import { createTRPCContext } from "~/server/api/trpc";
+import { createQueryClient } from "./query-client";
 
 /**
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
  * handling a tRPC call from a React Server Component.
  */
-const createContext = cache(() => {
-  const heads = new Headers(headers() as unknown as UnsafeUnwrappedHeaders);
+const createContext = cache(async () => {
+  const heads = new Headers(await headers());
   heads.set("x-trpc-source", "rsc");
 
   return createTRPCContext({
-    headers: heads,
+    headers: heads
   });
 });
 
@@ -26,5 +26,5 @@ const caller = createCaller(createContext);
 
 export const { trpc: apiS, HydrateClient } = createHydrationHelpers<AppRouter>(
   caller,
-  getQueryClient,
+  getQueryClient
 );
